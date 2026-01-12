baeaf208319bdd7c3af10505152fdc29a9cf09f2
Rohit <rohitshinde7922@gmail.com>
2026-01-13 00:11:55 +0530

    Second Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Brilliant Clone 🚀</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>Mini Brilliant</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
